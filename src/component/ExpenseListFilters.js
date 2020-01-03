ecb06f45325c888ad896f86f378a79b66b03ca66
Saerom Bang <saerombang11@gmail.com>
2020-01-03 15:56:56 +0900

    Sort by date and amount

--- src/component/ExpenseListFilters.js
+++ src/component/ExpenseListFilters.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {setTextFilter} from '../actions/filters';
-import { removeExpense } from '../actions/expenses';
-
+import {setTextFilter, sortByDate, sortByAmount} from '../actions/filters';
+ 
 const ExpenseListFilters = (props) => (
   <div>
     {/* when the input changes the onChange fires */}
@@ -12,7 +11,17 @@ const ExpenseListFilters = (props) => (
       props.dispatch(setTextFilter(e.target.value))
       // console.log(e.target.value)
     }}/>
-    <button onChange={props.removeExpense}>Remove</button>
+    <select value={props.filters.sortBy}
+      onChange={(e) => {
+        if(e.target.value === 'date'){
+          props.dispatch(sortByDate())
+        }else if (e.target.value === 'amount '){
+          props.dispatch(sortByAmount())
+        }
+      }}>
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
   </div>
 )
 
@@ -20,10 +29,10 @@ const ExpenseListFilters = (props) => (
 // so we have access to dispatch in out component so we can directly dispatch action?
 const mapStateToProps = (state) => {
   return {
-    filters: state.filters,
-    expenses: state.expenses
-
+    filters: state.filters
   }
 }
 
 export default connect(mapStateToProps)(ExpenseListFilters);
+
+
